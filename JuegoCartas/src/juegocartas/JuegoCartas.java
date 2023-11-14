f68f00b328035ab490848ce26c10fefbfbdac31d
Nicolas Castellanos <nicolasfelipe00234@gmail.com>
2023-11-14 11:57:27 -0500

    agregando funcionalidad de puntaje

--- JuegoCartas/src/juegocartas/JuegoCartas.java
+++ JuegoCartas/src/juegocartas/JuegoCartas.java
@@ -1,8 +1,7 @@
 package juegocartas;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import javax.swing.*;
 
 
@@ -42,24 +41,28 @@ public class JuegoCartas extends JFrame {
 
         JPanel panel = new JPanel();
 
-        int promedio1 = (int) (Math.random() * 302 + 290);
+        // Promedios de las cartas
+        int promedio1 = (int) (Math.random() * 320 + 280);
+        int promedio2 = (int) (Math.random() * 320 + 280);
+        int promedio3 = (int) (Math.random() * 320 + 280);
 
-        int promedio2 = (int) (Math.random() * 302 + 290);
 
-        int promedio3 = (int) (Math.random() * 302 + 290);
 
+
+        // Tamano de las cartas
         Dimension nuevoTamanio = new Dimension(122, 248);
 
+        // Tamano del label del ganador
         Dimension dimensionLabel = new Dimension(200, 50);
 
-
+        // Rutas de las cartas
         String rutaCarta1 = "C:\\Users\\Nicolas\\Documents\\Code\\Intellij\\juegodecartaslindo\\JuegoCartas\\src\\juegocartas\\imagenes\\imagen1.png";
-
         String rutaCarta2 = "C:\\Users\\Nicolas\\Documents\\Code\\Intellij\\juegodecartaslindo\\JuegoCartas\\src\\juegocartas\\imagenes\\imagen2.png";
-
         String rutaCarta3 = "C:\\Users\\Nicolas\\Documents\\Code\\Intellij\\juegodecartaslindo\\JuegoCartas\\src\\juegocartas\\imagenes\\imagen3.png";
 
+        // El titulo del juego
         JLabel titulo = new JLabel("Inicio del juego.");
+
 
         JLabel puntajeComputador = new JLabel(".");
         final JLabel[] puntajeJugador = {new JLabel(".")};
@@ -67,93 +70,28 @@ public class JuegoCartas extends JFrame {
         JLabel tituloCartaCompu = new JLabel("La carta con el texto verde es el computador");
         JLabel tempor = new JLabel("....");
 
-        JButton cartaJugador = new ImageButton("Num random: " + promedio1, rutaCarta1);
 
-        JButton cartaJugador2 = new ImageButton("Num random: " + promedio3, rutaCarta3);
+        JButton cartaDelComputador = new ImageButton("Num random: " + promedio1, rutaCarta1);
 
         JButton fusionar = new JButton("Fusionar!");
 
+        // El checkbox de la seleccion de la carta
         JCheckBox checkbox = new JCheckBox(" ");
         checkbox.setVisible(false);
         panel.add(checkbox);
 
-//        cartaJugador.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent e) {
-//                System.out.println("Carta Del Jugador seleccionada");
-//                cartaJugador.setVisible(false);
-//
-//                try {
-//                    Thread.sleep(2 * 1000);
-//                } catch (InterruptedException ex) {
-//                    throw new RuntimeException(ex);
-//                }
-//                checkbox.setSelected(true);
-//                if (checkbox.isSelected()) {
-//                    System.out.println("Carta seleccionada");
-//
-//
-//                    for (int i = 3; i > 0; i--) {
-//
-//                        try {
-//                            tempor.setText(String.valueOf(i));
-//                            System.out.println(i);
-//                            Thread.sleep(1 * 1000);
-//                        } catch (InterruptedException ex) {
-//                            throw new RuntimeException(ex);
-//                        }
-//
-//                    }
-//
-//
-//                    if (promedio1 > promedio2) {
-//                        System.out.println("Gano el computador");
-//                        JLabel res = new JLabel("Gano el computador");
-//                        puntajeComp[0] += 50;
-//                        System.out.println(puntajeComp);
-//                        res.setHorizontalAlignment(SwingConstants.CENTER);
-//                        res.setOpaque(true);
-//                        res.setBackground(Color.BLUE);
-//                        res.setPreferredSize(dimensionLabel);
-//
-//                        panel.add(res);
-//                    } else {
-//
-//                        System.out.println("Gano la carta del jugador");
-//                        JLabel res = new JLabel("Gano la carta del jugador");
-//                        puntajeJugador[0] += 50;
-//                        System.out.println(puntajeJugador);
-//
-//                        res.setHorizontalAlignment(SwingConstants.CENTER);
-//                        res.setOpaque(true);
-//                        res.setBackground(Color.RED);
-//                        res.setPreferredSize(dimensionLabel);
-//
-//                        panel.add(res);
-//
-//                    }
-//
-//                }
-//
-//                try {
-//                    Thread.sleep(2 * 1000);
-//                } catch (InterruptedException ex) {
-//                    throw new RuntimeException(ex);
-//                }
-//
-//                checkbox.setSelected(false);
-//            }
-//        });
-
+        // Los puntajes
         final int[] puntajeCompu = {0};
         final int[] puntajeJug = {0};
 
-        JButton cartaDelComputador = new ImageButton("Num random: " + promedio2, rutaCarta2);
-        cartaDelComputador.addActionListener(new ActionListener() {
 
+        // Primer carta del jugador
+        JButton cartaJugador = new ImageButton("Num random: " + promedio2, rutaCarta2);
+        cartaJugador.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Carta del computador presionada");
-                cartaDelComputador.setVisible(false);
+                System.out.println("Carta del jugador presionada");
+                cartaJugador.setVisible(false);
                 try {
                     Thread.sleep(2 * 1000);
                 } catch (InterruptedException ex) {
@@ -161,7 +99,19 @@ public class JuegoCartas extends JFrame {
                 }
                 checkbox.setSelected(true);
                 if (checkbox.isSelected()) {
-                    System.out.println("Carta seleccionada");
+//                    System.out.println("Carta seleccionada");
+
+                    for (int i = 3; i > 0; i--) {
+
+                        try {
+                            tempor.setText(String.valueOf(i));
+                            System.out.println(i);
+                            Thread.sleep(1000);
+                        } catch (InterruptedException ex) {
+                            throw new RuntimeException(ex);
+                        }
+
+                    }
 
                     if (promedio1 > promedio2) {
                         System.out.println("Gano el computador");
@@ -203,16 +153,85 @@ public class JuegoCartas extends JFrame {
             }
         });
 
+        // Segunda carta del jugador
+        JButton cartaJugador2 = new ImageButton("Num random: " + promedio3, rutaCarta3);
+        cartaJugador2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Carta del jugador presionada");
+                cartaJugador2.setVisible(false);
+                try {
+                    Thread.sleep(2 * 1000);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+                checkbox.setSelected(true);
+                if (checkbox.isSelected()) {
+//                    System.out.println("Carta seleccionada");
+
+                    for (int i = 3; i > 0; i--) {
+
+                        try {
+                            tempor.setText(String.valueOf(i));
+                            System.out.println(i);
+                            Thread.sleep(1000);
+                        } catch (InterruptedException ex) {
+                            throw new RuntimeException(ex);
+                        }
+
+                    }
+
+                    if (promedio1 > promedio3) {
+                        System.out.println("Gano el computador");
+                        JLabel res = new JLabel("Gano el computador");
+                        puntajeCompu[0] += 50;
+                        System.out.println(puntajeCompu[0]);
+
+                        res.setHorizontalAlignment(SwingConstants.CENTER);
+                        res.setOpaque(true);
+                        res.setBackground(Color.BLUE);
+                        res.setPreferredSize(dimensionLabel);
+
+                        panel.add(res);
+                    } else {
+
+                        System.out.println("Gano la carta del jugador");
+                        JLabel res = new JLabel("Gano la carta del jugador");
+                        puntajeJug[0] += 50;
+                        System.out.println(puntajeJug[0]);
+
+                        res.setHorizontalAlignment(SwingConstants.CENTER);
+                        res.setOpaque(true);
+                        res.setBackground(Color.RED);
+                        res.setPreferredSize(dimensionLabel);
+
+                        panel.add(res);
+
+                    }
+
+                }
+
+                try {
+                    Thread.sleep(2 * 1000);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                checkbox.setSelected(false);
+            }
+        });
+
+        // Agregar las cartas
+        cartaDelComputador.setPreferredSize(nuevoTamanio);
+        panel.add(cartaDelComputador);
 
         cartaJugador.setPreferredSize(nuevoTamanio);
         panel.add(cartaJugador);
 
-        cartaJugador.setPreferredSize(nuevoTamanio);
+        cartaJugador2.setPreferredSize(nuevoTamanio);
         panel.add(cartaJugador2);
 
-        cartaDelComputador.setPreferredSize(nuevoTamanio);
-        panel.add(cartaDelComputador);
 
+        // Agregar las otras cosas
         panel.add(titulo);
         panel.add(fusionar);
         panel.add(tituloCartaCompu);
